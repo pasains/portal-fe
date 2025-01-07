6ccaababf2366d1bfa932270ef31b5fe0e32b000
Susilawati <susilawatibudiutami@gmail.com>
2025-01-07 16:11:54 +0700

    feat: activated search button

--- src/container/upperTable/index.tsx
+++ src/container/upperTable/index.tsx
@@ -1,72 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  ArrowDownTrayIcon,
+  MagnifyingGlassIcon,
+  PencilIcon,
+} from "@heroicons/react/24/solid";
+import {
+  Button,
+  CardHeader,
+  Input,
+  Typography,
+} from "@material-tailwind/react";
 import { useNavigate } from "react-router-dom";
 
 const UpperTable = ({
   pageTitle,
   description,
+  handleInputSearch,
   createTitle,
   createLink,
   handleDownload,
+  searchQuery,
 }: any) => {
   const navigate = useNavigate();
+
   return (
     <div className="relative z-0 flex flex-col w-full h-full text-black bg-white shadow-md bg-clip-border">
-      <div className="relative mx-4 my-6 overflow-hidden text-black bg-white rounded-none bg-clip-border">
-        <div className="flex flex-col justify-between gap-8 md:flex-row md:items-center">
-          <div className="space-y-2">
-            <h1 className="font-semibold leading-snug tracking-normal text-slate-800 w-full text-xl max-w-lg lg:max-w-2xl lg:text-3xl">
+      <CardHeader floated={false} shadow={false} className="rounded-none">
+        <div className="mb-4 flex flex-col justify-between gap-8 md:flex-row md:items-center">
+          <div>
+            <Typography variant="h4" color="blue-gray">
               {pageTitle}
-            </h1>
-            <h2>
-              <p className="font-light leading-snug tracking-normal text-slate-800 mx-auto w-full text-sm max-w-xs lg:max-w-md lg:text-lg">
-                {description}
-              </p>
-            </h2>
+            </Typography>
+            <Typography color="gray" className="mt-1 font-normal">
+              {description}
+            </Typography>
           </div>
-          <div className="flex flex-row h-10 w-full gap-3 shrink-0 md:w-max">
-            <button
-              onClick={() => {
-                navigate(createLink);
-              }}
-              className="flex select-none items-center gap-3 rounded-lg bg-gray-900 py-3 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-              type="button"
+          <div className="flex w-full shrink-0 gap-2 md:w-max">
+            <div className="w-full md:w-72">
+              <Input
+                label="Search"
+                icon={<MagnifyingGlassIcon className="h-5 w-5" />}
+                value={searchQuery}
+                onChange={handleInputSearch}
+              />
+            </div>
+            <Button
+              className="flex items-center gap-3 bg-black"
+              onClick={() => navigate(createLink)}
+              size="sm"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                aria-hidden="true"
-                className="w-3 h-3"
-              >
-                <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z"></path>
-              </svg>
-              {createTitle}
-            </button>
-            <button
-              className="flex select-none items-center w-fit gap-3 rounded-lg bg-gray-900 py-3 px-4 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-              type="button"
+              <PencilIcon strokeWidth={2} className="h-4 w-4" /> {createTitle}
+            </Button>
+            <Button
+              className={`flex items-center gap-3 bg-black`}
               onClick={handleDownload}
+              size="sm"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                aria-hidden="true"
-                className="w-4 h-4"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
-                ></path>
-              </svg>
-              Download
-            </button>
+              <ArrowDownTrayIcon strokeWidth={2} className="h-4 w-4" /> Download
+            </Button>
           </div>
         </div>
-      </div>
+      </CardHeader>
     </div>
   );
 };
--- src/hooks/inventoryGroup/inventoryGroupDetail/index.tsx
+++ src/hooks/inventoryGroup/inventoryGroupDetail/index.tsx
@@ -121,11 +121,29 @@ export function useInventoryGroupDetail() {
     // Export the workbook to an Excel file
     XLSX.writeFile(workbook, "inventory.xlsx");
   };
+  const handleSearch = async (query: string) => {
+    try {
+      const response = await fetch(
+        `${REACT_APP_PORTAL_BE_URL}/api/inventory?inventoryGroupId${id}&search=${query}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${token}`,
+          },
+        },
+      );
+      const json = await response.json();
+      setInventoryItems(json.data.inventory); // Assuming the response has an `inventory` array
+    } catch (error) {
+      console.error("Error fetching inventory:", error);
+    }
+  };
 
   return {
     id,
     page,
     totalPage,
+    handleSearch,
     handleDownload,
     setPage,
     inventoryItems,
